895786cfcbe2a53d9b08ca08d65d8523d5505c5c
Moritz Zimmer <moritz.zmmr@gmail.com>
2018-08-14 09:41:34 +0200

    Added simple package with test

--- func/schedule/main.go
+++ func/schedule/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+
+	"github.com/spring-media/lambda-blueprint/greeter"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
+// see https://docs.aws.amazon.com/lambda/latest/dg/go-programming-model.html for details
 func main() {
 	lambda.Start(handler)
 }
 
-func handler() error {
-	fmt.Println("Hello World")
+func handler(ctx context.Context) error {
+	lc, _ := lambdacontext.FromContext(ctx)
+	log.Printf("%s", greeter.Greet(lc.AwsRequestID))
 	return nil
 }
