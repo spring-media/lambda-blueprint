df222aad54aafe7218f2135e6667f98f0a7c914c
Moritz Zimmer <moritz.zmmr@gmail.com>
2018-08-15 12:59:25 +0200

    refactored to policy data ressources

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,58 +1,54 @@
 resource "aws_lambda_function" "lambda" {
   function_name = "${var.function_name}"
   description   = "${var.description}"
+  s3_bucket     = "${var.s3_bucket}"
+  s3_key        = "${var.s3_key}"
+  role          = "${aws_iam_role.lambda.arn}"
+  runtime       = "${var.runtime}"
+  handler       = "${var.handler}"
+  timeout       = "${var.timeout}"
+  memory_size   = "${var.memory_size}"
+}
 
-  s3_bucket = "${var.s3_bucket}"
-  s3_key    = "${var.s3_key}"
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-  role        = "${aws_iam_role.lambda.arn}"
-  runtime     = "${var.runtime}"
-  handler     = "${var.handler}"
-  timeout     = "${var.timeout}"
-  memory_size = "${var.memory_size}"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role" "lambda" {
-  name = "${var.function_name}"
+  name = "${var.function_name}-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
-resource "aws_iam_policy" "cloudwatch-logs-policy" {
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
+data "aws_iam_policy_document" "cloudwatch_logs" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "cloudwatch-logs-policy-attachment" {
+resource "aws_iam_policy" "cloudwatch_logs_policy" {
+  name        = "${var.function_name}-cloudwatch-logs"
+  description = "Provides minimum CloudWatch Logs permissions for ${var.function_name}."
+  policy      = "${data.aws_iam_policy_document.cloudwatch_logs.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy_attachment" {
   role       = "${aws_iam_role.lambda.name}"
-  policy_arn = "${aws_iam_policy.cloudwatch-logs-policy.arn}"
+  policy_arn = "${aws_iam_policy.cloudwatch_logs_policy.arn}"
 }
 
 resource "aws_cloudwatch_log_group" "lambda" {
