8e5844ade6a29b25798761ee23f50abf8bf1293e
Moritz Zimmer <moritzzimmer@users.noreply.github.com>
2018-08-17 22:10:03 +0200

    Use remote instead of local modules for api and lambda (#12)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "api-lambda" {
-  source        = "./modules/lambda"
+  source        = "github.com/spring-media/terraform-aws-lambda"
   name          = "api-lambda"
   description   = "Example Lambda with API Gateway event."
   function_name = "lambda-blueprint-fct"
@@ -9,7 +9,7 @@ module "api-lambda" {
 }
 
 module "api" {
-  source            = "./modules/api"
+  source            = "github.com/spring-media/terraform-aws-api-gateway"
   name              = "Lambda-Blueprint-Example"
   stage_name        = "test"
   lambda_arn        = "${module.lambda.arn}"
@@ -17,15 +17,25 @@ module "api" {
 }
 
 module "lambda" {
-  source                  = "./modules/lambda"
+  source                  = "github.com/spring-media/terraform-aws-lambda"
   name                    = "lambda"
   description             = "Example Lambda with a stream event source mapping."
   function_name           = "lambda-blueprint-stream-fct"
   handler                 = "dynamodb"
   s3_bucket               = "${var.s3_bucket}"
   s3_key                  = "${var.version}/dynamodb.zip"
-  stream_event_source_arn = "${aws_dynamodb_table.dynamodb.stream_arn}"
   stream_enabled          = true
+  stream_event_source_arn = "${aws_dynamodb_table.dynamodb.stream_arn}"
+}
+
+module "scheduled-lambda" {
+  source              = "github.com/spring-media/terraform-aws-lambda"
+  name                = "scheduled-lambda"
+  function_name       = "my-scheduled-function"
+  handler             = "my-handler"
+  s3_bucket           = "some-s3-bucket"
+  s3_key              = "some-s3-key"
+  schedule_expression = "rate(1 minute)"
 }
 
 resource "aws_dynamodb_table" "dynamodb" {
