d72ecc3c94ba602d09538b631c5f7d18e1e2e9b0
Moritz Zimmer <moritz.zmmr@gmail.com>
2018-08-19 07:19:28 +0200

    Do not prefix with random

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,7 @@
-resource "random_string" "prefix" {
-  length  = 5
-  special = false
-}
-
 module "lambda" {
   source        = "github.com/spring-media/terraform-aws//lambda"
   name          = "lambda"
-  function_name = "${random_string.prefix.result}-lambda-blueprint-scheduled-fct"
+  function_name = "lambda-blueprint-scheduled-fct"
   handler       = "scheduled"
   s3_bucket     = "${var.s3_bucket}"
   s3_key        = "${var.version}/scheduled.zip"
