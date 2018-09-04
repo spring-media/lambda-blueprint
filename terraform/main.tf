ba627cb677b414899fd0ca1e475321abd25d254f
Moritz Zimmer <moritz.zmmr@gmail.com>
2018-09-04 15:17:29 +0200

    Use new lambda module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
 module "lambda" {
-  source        = "github.com/spring-media/terraform-aws//lambda"
-  name          = "scheduled"
-  function_name = "lambda-blueprint-scheduled-fct"
-  s3_bucket     = "${var.s3_bucket}"
-  s3_key        = "${var.version}/scheduled.zip"
+  source              = "github.com/spring-media/terraform-aws-lambda"
+  name                = "scheduled"
+  function_name       = "lambda-blueprint-scheduled-fct"
+  s3_bucket           = "${var.s3_bucket}"
+  s3_key              = "${var.version}/scheduled.zip"
+  schedule_expression = "rate(1 minute)"
 }
