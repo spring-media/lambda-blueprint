e08c027895e6bd788d6af3ec49e53b79740352ac
Moritz Zimmer <moritz.zmmr@gmail.com>
2018-11-05 13:17:01 +0100

    Updated blueprint to latest modul version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,14 @@
 module "lambda" {
-  source              = "github.com/spring-media/terraform-aws-lambda"
-  version             = ">= 1.2.0"
-  name                = "scheduled"
-  function_name       = "lambda-blueprint-scheduled-fct"
-  s3_bucket           = "${var.s3_bucket}"
-  s3_key              = "${var.version}/scheduled.zip"
-  schedule_expression = "rate(1 minute)"
+  source        = "github.com/spring-media/terraform-aws-lambda?ref=v2.0.0"
+  name          = "scheduled"
+  function_name = "lambda-blueprint-stream-fct"
+  s3_bucket     = "${var.s3_bucket}"
+  s3_key        = "${var.version}/scheduled.zip"
+
+  event {
+    type                = "cloudwatch-event"
+    schedule_expression = "rate(1 minute)"
+  }
 
   tags {
     service    = "my-service"
